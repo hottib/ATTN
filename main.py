86304f79d39733bb59e9854460293e88a68dd7e4
Holger H <holgheiss@gmail.com>
2023-03-27 21:29:42 +0200

    conceptual main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+alloziere speicher für environment (messungen usw)
+while True:
+  # camera_input: bekomme state und rewardwert
+  # model:        iteriere netzwerk (berechne aktion und optimiere)
+  # actions:      führe berechnete aktion aus
+"""
